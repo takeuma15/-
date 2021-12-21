f9c441f911363acd72fb67fff54bf539c9b3570f
takeuma15 <zm19395@josai.ac.jp>
2021-12-21 02:03:04 +0000

    12/21

--- ex6-11.js
+++ ex6-11.js
@@ -2,7 +2,6 @@ function hyoji()
 {var str="一文字ずつ表示します。"; 
 var cnt = document.timer.moji.value.length;
 if(cnt<11){document.timer.moji.value=str.substr(0,cnt+1);}
-else{document.timer.moji.value="";}
-setTimeout("hyoji()",3000);}
-function startfnc(){setInterval("hyoji()",3000);}
+else{document.timer.moji.value="";}}
+function stratfnc(){setInterval("hyoji()",1000);}
 function mes(){alert("３秒経ちました!");}
