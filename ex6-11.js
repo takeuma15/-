813d77d2dff7fded784af259b08938d5a3502e98
takeuma15 <zm19395@josai.ac.jp>
2022-01-18 01:45:12 +0000

    0118

--- ex6-11.js
+++ ex6-11.js
@@ -3,5 +3,5 @@ function hyoji()
 var cnt = document.timer.moji.value.length;
 if(cnt<11){document.timer.moji.value=str.substr(0,cnt+1);}
 else{document.timer.moji.value="";}}
-function stratfnc(){setInterval("hyoji()",1000);}
+function startfnc(){setInterval("hyoji()",1000);}
 function mes(){alert("３秒経ちました!");}
--- ex6-12.js
+++ ex6-12.js
@@ -1,6 +1,6 @@
 function hyoji2()
 {var str="一文字ずつ表示します。"; 
-var cnt = document.timer.moji1.value.length;
-if(cnt<11){document.timer.moji1.value=str.substr(0,cnt+1);}
-else{document.timer.moji1.value="";}
-setTimeout("hyoji()",1000);}
+var cnt = document.timer1.moji1.value.length;
+if(cnt<11){document.timer1.moji1.value=str.substr(0,cnt+1);}
+else{document.timer1.moji1.value="";}
+setTimeout("hyoji2()",1000);}
